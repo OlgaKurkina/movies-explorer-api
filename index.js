19836c7bf09195816b8f755a1d00d1040c5312e8
Olga Kurkina <doktorovaoe@gmail.com>
2023-12-04 18:22:31 +0300

    Fix validation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const { errors } = require('celebrate');
-const cors = require('cors');
-const { limiter } = require('./middlewares/limiter');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const { errors } = require("celebrate");
+const cors = require("cors");
+const { limiter } = require("./middlewares/limiter");
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const router = require('./routes/router');
-const { requestLogger, errorLogger } = require('./middlewares/Logger');
+const router = require("./routes/router");
+const { requestLogger, errorLogger } = require("./middlewares/Logger");
 
-const errorCheck = require('./middlewares/errorCheck');
-const { MONGO_DB } = require('./utils/config');
+const errorCheck = require("./middlewares/errorCheck");
+const { MONGO_DB } = require("./utils/config");
 
 mongoose.connect(MONGO_DB);
 
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,5 +1,5 @@
-const { celebrate, Joi } = require('celebrate');
-const { regex } = require('../utils/constants');
+const { celebrate, Joi } = require("celebrate");
+const { regex } = require("../utils/constants");
 
 const loginValidation = celebrate({
   body: Joi.object().keys({
@@ -19,7 +19,7 @@ const registerValidation = celebrate({
 const userDataValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().email(),
   }),
 });
 
@@ -39,9 +39,9 @@ const movieDataValifation = celebrate({
     image: Joi.string().required().regex(regex),
     trailerLink: Joi.string().required().regex(regex),
     thumbnail: Joi.string().required().regex(regex),
-    movieId: Joi.string().required(),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required().min(2).max(50),
+    nameEN: Joi.string().required().min(2).max(50),
   }),
 });
 
